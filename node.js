1c13e43dc0b6de43ff7ab8f51f79277fd85c11d3
Pustam-Rajbanshi <pustamraj109@gmail.com>
2025-02-09 10:24:45 +0545

    Add files via upload

---
+++ node.js
@@ -0,0 +1,18 @@
+
+
+
+let fullname= "pustam"
+isfollow = false;
+let x;
+let y = null;
+let z =BigInt("123");
+let p =Symbol("hello");
+let a=5;
+let b=2;
+console.log("a=",a," b =",b);
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
